c9229798558e1e1525eda3fce38e4105149cf135
Sam Turner <samt@thoriumcube.org>
2024-09-04 18:55:18 +0100

    Add Employee

---
+++ src/main/java/com/so32/emsbackend/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.so32.emsbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+}
