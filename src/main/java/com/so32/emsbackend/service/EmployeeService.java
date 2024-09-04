72ee4660766facc1d16395ebd0c3854c2956cad6
Sam Turner <samt@thoriumcube.org>
2024-09-04 18:55:40 +0100

    Add EmployeeService interface

---
+++ src/main/java/com/so32/emsbackend/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.so32.emsbackend.service;
+
+import com.so32.emsbackend.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto employeeDto);
+
+    void deleteEmployee(Long employeeId);
+
+}
